fce08a9484d7278740151f4804ce5e9080d5e0b0
Moose <kalenjonathan@hotmail.com>
2025-12-05 12:58:52 -0600

    yeppa

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -1,31 +1,82 @@
+using System.Collections;
 using UnityEngine;
+using Unity.Netcode;
+using Unity.VisualScripting;
 
 [CreateAssetMenu(fileName = "Weapon", menuName = "Scriptable Objects/Weapon")]
 public class Weapon : ScriptableObject
 {
     [SerializeField] private Mesh weaponMesh;
-    [SerializeField] private Projectile projectilePrefab;
+    [SerializeField] private GameObject projPrefab;
 
-    [SerializeField] private float damage = 1f;
-    [SerializeField] private float strength = 1f;
-    [SerializeField] private float range = 1f;
-    [SerializeField] private float speed = 1f;
-    [SerializeField] private float fireRate = 1f;
+    [SerializeField] public float damage = 1f;
+    [SerializeField] public float strength = 1f;
+    [SerializeField] public float range = 1f;
+    [SerializeField] public float speed = 1f;
+    [SerializeField] public float fireRate = 1f;
+    [SerializeField] public float lifeTime = 10f;
+    [SerializeField] public float radius = 1f;
 
-    public void Awake()
-    {
-        projectilePrefab.SetStats(speed, strength, range);
-    }
     public void Shoot(Transform shootPoint, Vector3 direction)
     {
-        if(projectilePrefab == null)
+        GameObject obj = Instantiate(projPrefab, shootPoint.position + direction, Quaternion.identity);
+        obj.AddComponent<Projectile>();
+
+        if(obj == null)
         {
-            Debug.LogWarning("No projectile prefab assigned to weapon " + name);
+            Debug.Log("how");
             return;
         }
-
-        projectilePrefab.GetComponent<Projectile>().direction = direction;
-        Projectile projectile = Instantiate(projectilePrefab, shootPoint.position + direction * 1.1f, Quaternion.identity);
+        obj.GetComponent<Projectile>().Init(this, direction);
     }
 
+}
+
+class Projectile : NetworkBehaviour
+{
+    private Collider[] colliders;
+    private GameObject ground;
+
+    private Weapon weapon;
+    private Vector3 direction;
+    private IEnumerator die()
+    {
+        yield return new WaitForSeconds(weapon.lifeTime);
+        Destroy(gameObject);
+    }
+    public void Init(Weapon weapon, Vector3 direction)
+    {
+        this.weapon = weapon;
+        this.direction = direction;
+        ground.layer = LayerMask.NameToLayer("Ground");
+
+        if(weapon == null)
+        {
+            Debug.Log("weapon is null");
+            Destroy(gameObject);
+        }
+
+        StartCoroutine(die());
+    }
+    void Update()
+    {
+        transform.Translate(weapon.speed * Time.deltaTime * direction);
+        colliders = Physics.OverlapSphere(transform.position, weapon.radius);
+        foreach (Collider hit in colliders)
+        {
+            if (hit != null)
+            {
+                Debug.Log(LayerMask.Equals(hit.gameObject.layer, ground.layer) + " | " + hit.gameObject.layer + " | " + ground.layer);
+                if (hit.gameObject.CompareTag("Player"))
+                {
+                    hit.gameObject.GetComponent<PlayerMovement2>()?.AddForce(direction * weapon.strength);
+                    hit.gameObject.GetComponent<Player>()? .TakeDamage(weapon.damage);
+                    Destroy(gameObject);
+                } else if (LayerMask.Equals(hit.gameObject.layer, ground.layer))
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
 }
